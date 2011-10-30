7073af9f34fa8bf9f9eec23a0f6a2cce60ddc729
Kevin Ying <ying.kev@gmail.com>
2011-10-31 10:32:49 +1100

    testfiles from assignment marker

--- test02.cpp
+++ test02.cpp
@@ -1,26 +1,53 @@
-#include <algorithm>
 #include <iostream>
 #include <iterator>
-
+#include <string>
+#include <algorithm>
 #include "btree.h"
 
-void foo(const btree<int> &b) {
-  std::copy(b.begin(), b.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+using namespace std;
+
+template <typename T>
+void find_in_tree(const btree<T> &b, T val) {
+  typename btree<T>::const_iterator iter =
+    find(b.begin(), b.end(), val);
+  if (iter == b.end()) 
+    cout << val << " not found" << endl;
+  else
+    cout << val << " found" << endl;
 }
 
-int main(void) {
-  btree<int> b;
-
-  b.insert(1);
-  b.insert(10);
-  b.insert(3);
-  b.insert(4);
-
-  for(btree<int>::iterator iter = b.begin(); iter != b.end(); ++iter)
-    std::cout << *iter << std::endl;
-
-  foo(b);
+int main() {
+  btree<string> bs(3);
   
-  return 0;
+  bs.insert("comp3000");
+  bs.insert("comp3171");
+  bs.insert("comp2000");
+  bs.insert("comp1000");
+  bs.insert("acomp3000");
+  bs.insert("acomp3171");
+  bs.insert("acomp2000");
+  bs.insert("bcomp1000");
+  bs.insert("ccomp3000");
+  bs.insert("dcomp3171");
+  bs.insert("ecomp2000");
+  bs.insert("fcomp1000");
+  
+  find_in_tree(bs, string("comp3171"));
+  
+  btree<int> bi(3);
+  
+  bi.insert(1);
+  bi.insert(10);
+  bi.insert(3);
+  bi.insert(4);
+  bi.insert(11);
+  bi.insert(110);
+  bi.insert(13);
+  bi.insert(14);
+  bi.insert(-1);
+  bi.insert(-10);
+  bi.insert(-3);
+  bi.insert(-4);
+  
+  find_in_tree(bi, 100);
 }
--- test03.cpp
+++ test03.cpp
@@ -1,37 +1,49 @@
-#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <string>
-
 #include "btree.h"
 
-template <typename T>
-void find_in_tree(const btree<T> &b, T val) {
-  typename btree<T>::const_iterator iter = std::find(b.begin(), b.end(), val);
-  if (iter == b.end()) 
-    std::cout << val << " not found" << std::endl;
-  else
-    std::cout << val << " found" << std::endl;
+using namespace std;
+
+class X {
+public:
+  friend ostream& operator<<(ostream &, const X &);
+  X(): i(0) { }
+  X(int ii) : i(ii) { }
+  bool operator<(const X& x) const { return i < x.i; }
+  bool operator==(const X& x) const { return i == x.i; }
+  bool operator>(const X& x) const { return i > x.i; }
+  bool operator!=(const X& x) const { return i != x.i; }
+  bool operator<=(const X& x) const { return i <= x.i; }
+  bool operator>=(const X& x) const { return i >= x.i; }
+private:
+  int i;
+};
+
+ostream& operator<<(ostream &out, const X& x) {
+  cout << x.i;
+  return out;
 }
 
-int main(void) {
-  btree<std::string> bts;
-
-  bts.insert("comp3000");
-  bts.insert("comp3171");
-  bts.insert("comp2000");
-  bts.insert("comp1000");
-
-  find_in_tree(bts, std::string("comp3171"));
-
-  btree<int> bti;
-
-  bti.insert(1);
-  bti.insert(10);
-  bti.insert(3);
-  bti.insert(4);
-
-  find_in_tree(bti, 100);
-
-  return 0;
+int main() {
+  btree<X> b(4);
+  
+  b.insert(X(2));
+  b.insert(X(200));
+  b.insert(X(50));
+  b.insert(X(4));
+  b.insert(X(2));
+  b.insert(X(1200));
+  b.insert(X(150));
+  b.insert(X(14));
+  b.insert(X(12));
+  b.insert(X(11200));
+  b.insert(X(-150));
+  b.insert(X(-14));
+  b.insert(X(212));
+  b.insert(X(-200));
+  b.insert(X(-50));
+  b.insert(X(-4));
+  for(btree<X>::iterator iter = b.begin(); iter != b.end(); ++iter)
+    cout << *iter << endl;
 }
